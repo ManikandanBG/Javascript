9e7157c5f09138739e8d4ff4581ad3bf0096d97a
jagann8 <jagancloudlab@gmail.com>
2022-03-25 11:13:32 +0530

    js code

---
+++ 3.es-6/2.JSES6/script.js
@@ -0,0 +1,47 @@
+
+
+/**
+  primitive data types:
+  ========================
+     1.string
+     2.number
+     3.bigint
+     4.undefined
+     5.boolean
+    
+  Reference data types:
+  ========================
+     Objects
+
+
+ */
+
+//    var v;
+//    console.log(v)
+
+
+//    string interpolation 
+
+
+// var name="jagan";
+// var clgName="PEC";
+
+
+// console.log(`My name is ${name} and my college name is ${clgName}`)
+
+// number
+
+// var value=5.2;
+// console.log(typeof value);
+
+// false values :
+
+// false,0,"",NaN,null,undefined 
+
+// bigint
+
+
+const v=18n
+
+console.log(typeof v);
+
